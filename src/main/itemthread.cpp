0be49884bb950d9112075b2c41fd0f3d89e511d1
Maxim Shvecov <Gris87@yandex.ru>
2013-02-01 11:43:39 +0400

    Checking files in threads

--- src/main/itemthread.cpp
+++ src/main/itemthread.cpp
@@ -1,5 +1,7 @@
 #include "itemthread.h"
 
+#include <QFile>
+
 #include "filetreewidgetitem.h"
 
 ItemThread::ItemThread(const QString &aFileName, FileTreeWidgetItem *aItem, QObject *parent) :
@@ -7,14 +9,32 @@ ItemThread::ItemThread(const QString &aFileName, FileTreeWidgetItem *aItem, QObj
 {
     mFileName=aFileName;
     mItem=aItem;
-    mOk=false;
+    mOk=true;
 
     connect(this, SIGNAL(finished()), this, SLOT(threadFinished()));
 }
 
 void ItemThread::run()
 {
+    QFile aFile(mFileName);
 
+    if (aFile.exists())
+    {
+        aFile.open(QIODevice::ReadOnly);
+
+        while (!aFile.atEnd())
+        {
+            QString aLine=QString::fromUtf8(aFile.readLine());
+
+            if (!aLine.startsWith("+"))
+            {
+                mOk=false;
+                break;
+            }
+        }
+
+        aFile.close();
+    }
 }
 
 void ItemThread::threadFinished()
